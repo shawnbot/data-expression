d380e4a618f1be5f3b6df7bbed854075e438f8a6
Shawn Allen <shawn.allen@gsa.gov>
2015-12-09 17:30:10 -0800

    updated tests

--- test/index.js
+++ test/index.js
@@ -15,28 +15,27 @@ describe('evaluate()', function() {
   });
 });
 
-describe('evalutor()', function() {
+describe('evalutor', function() {
   it('returns a function', function() {
-    var e = datex.evaluator('foo + 1');
+    var e = datex('foo + 1');
     assert.equal(typeof e, 'function');
   });
 
   it('evaluates expressions with data', function() {
-    var e = datex.evaluator('foo + 1');
+    var e = datex('foo + 1');
     assert.equal(e({foo: 100}), 101);
   });
 
   it('returns undefined with bad data', function() {
-    var e = datex.evaluator('foo + 1');
+    var e = datex('foo + 1');
     assert.strictEqual(e(null), undefined);
   });
-});
+})
 
 describe('contexts', function() {
   it('create setters', function() {
-    var set = datex('a = b + 1');
-    var data = {b: 2};
-    assert.strictEqual(set(data), data);
+    var set = datex.setter('a = b + 1');
+    var data = set({b: 2});
     assert.equal(data.a, 3);
   });
 
@@ -56,59 +55,49 @@ describe('contexts', function() {
     assert.equal(c({foo: 0}), -1);
   });
 
-  it('can require modules', function() {
-    var c = datex('curry(function(x, y) { return x + y; })(99)(2)')
-      .require('curry');
-    assert.equal(c(), 101);
+  describe('require()', function() {
+    it('can require modules', function() {
+      var c = datex('curry(function(x, y) { return x + y; })(99)(2)')
+        .require('curry');
+      assert.equal(c(), 101);
+    });
+
+    it('can require aliased modules', function() {
+      var c = datex('c(function(x, y) { return x + y; })(99)(2)')
+        .require('c=curry');
+      assert.equal(c(), 101);
+    });
+
+    it('can require relative modules', function() {
+      // XXX note: this path is relative to the cwd, not __dirname
+      var c = datex('x2(x)')
+        .require('x2=./test/x2');
+      assert.equal(c({x: 21}), 42);
+    });
   });
 
-  it('can require Math methods', function() {
-    var c = datex('Math.pow(2, 2)')
-      .require('Math');
+  it('can access Math global', function() {
+    var c = datex('Math.pow(2, 2)');
     assert.strictEqual(c(), 4);
   });
 
-  it('can require Array methods', function() {
-    var c = datex('Array.prototype.reverse.call([1, 2, 3])')
-      .require('Array');
+  it('can require Array global', function() {
+    var c = datex('Array.prototype.reverse.call([1, 2, 3])');
     assert.deepEqual(c(), [3, 2, 1]);
   });
 });
 
 describe('map()', function() {
   it('creates maps with object expressions', function() {
-    var map = datex.map('{foo: bar, bar: baz}');
+    var map = datex('{foo: bar, bar: baz}');
     assert.deepEqual(map({bar: 1, baz: 2}), {foo: 1, bar: 2});
   });
 
   it('creates maps with key/value pairs', function() {
-    var map = datex.map({
+    var map = datex({
       foo: 'bar + 1',
       bar: 'baz + 2'
     });
     assert.deepEqual(map({bar: 1, baz: 2}), {foo: 2, bar: 4});
-  });
-
-  it('can set keys', function() {
-    var map = datex.map({
-      foo: 'bar + 1',
-      bar: 'baz + 2'
-    })
-    .key('qux', 'bar + 2');
-    // FIXME: should keys be set in the context as they're mapped?
-    assert.deepEqual(map({bar: 1, baz: 2}), {foo: 2, bar: 4, qux: 3});
-  });
-});
-
-describe('ES6', function() {
-  it('parses arrow functions', function() {
-    var a = esprima.parse('var a = function(x) { return x + 1; }').body[0].expression,
-        b = datex.parse('var a = x => x + 1;');
-    assert.deepEqual(a, b);
-  });
-
-  it('evaluates arrow functions', function() {
-    var c = datex.evaluate('[1, 2, 3].map(x => x + 1)');
-    assert.deepEqual(c, [2, 3, 4]);
   });
 });
